23c61a6b266eff6fde9d1588cb66a1b99ebe27f2
farhannfdl <farhan972015@gmail.com>
2022-06-18 08:24:35 +0700

    test newcollection

---
+++ src/api/newcollection/controllers/newcollection.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  newcollection controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::newcollection.newcollection');
---
+++ src/api/newcollection/services/newcollection.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newcollection service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::newcollection.newcollection');
